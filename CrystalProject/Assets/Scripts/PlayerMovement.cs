5d333b7a6521d9ddb9ab624b216bd217871f4138
AdamShohaieb <ashohaieb12@gmail.com>
2024-04-26 16:15:30 +0100

    AddedScript

---
+++ CrystalProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+
+    private void Update()
+    {
+        ProcessInput();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    public void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    private void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
